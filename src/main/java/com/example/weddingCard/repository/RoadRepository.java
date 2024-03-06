9b56432b82285d80bb9f7133c3330e4eee982fc3
birdnew1 <b1rdn2w@naver.com>
2024-03-06 18:44:38 +0900

    feat: POST /save/information Road table 중복 검사 추가
    information 중복 검사를 기반. weddingId로 조회. 새로운 Id일 경우 추가. 중복일 경우 업데이트

--- src/main/java/com/example/weddingCard/repository/RoadRepository.java
+++ src/main/java/com/example/weddingCard/repository/RoadRepository.java
@@ -1,7 +1,12 @@
 package com.example.weddingCard.repository;
 
+import com.example.weddingCard.entity.Information;
 import com.example.weddingCard.entity.Road;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoadRepository extends JpaRepository<Road, Integer> {
+
+    List<Road> findByWeddingId(Information information);
 }
